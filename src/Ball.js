52089d4f82caeb7a3d1ab4f0e747db0aea990d0e
korchel <korchel21@gmail.com>
2024-03-20 15:44:31 +0300

    push added, angle error

--- src/Ball.js
+++ src/Ball.js
@@ -1,12 +1,12 @@
 
 class Ball {
-    constructor(context, canvas, position, speed, color) {
+    constructor(context, canvas, position, color) {
       this.canvas = canvas;
       this.context = context
       this.color = color;
       this.position = position;
-      this.radius = 10;
-      this.speed = speed;
+      this.radius = 100;
+      this.speed = 0.1;
       // this.elasticity 
     }
 
@@ -17,7 +17,7 @@ class Ball {
         this.context.fill();
         this.context.closePath();
     }
-  static getDistance(pos1, pos2) {
+  getDistance(pos1, pos2) {
     const x1 = pos1.x;
     const y1 = pos1.y;
     const x2 = pos2.x;
@@ -25,9 +25,26 @@ class Ball {
     return Math.sqrt(Math.pow(y2 - y1, 2) + Math.pow(x2 - x1, 2));
   }
 
-  move() {
-    this.position.x += this.speed.x;
-    this.position.y += this.speed.y;
+  detectCollision(balls) {
+    for (let i = 0; i < balls.length; i += 1) {
+      if (this === balls[i]) continue;
+      if (this.getDistance(this.position, balls[i].position) < this.radius + balls[i].radius) {
+        console.log('collision!')
+      }
+    }
+  }
+
+  move(push) {
+    const { x1 = 0, x2 = 0, y1 = 0, y2 = 0 } = push;
+    if (x1 < this.position.x + this.radius
+      && x1 > this.position.x - this.radius
+      && y1 < this.position.y + this.radius
+      && y1 > this.position.y - this.radius
+    ) {
+      const pushAngle = Math.atan2(x2 - x1, y2 - y1);
+      this.position.x += this.speed.x * Math.cos(pushAngle)
+      this.position.y += this.speed.y * Math.sin(pushAngle)
+    }
     if (this.position.x + this.speed.x > this.canvas.width - this.radius ||
     this.position.x + this.speed.x < this.radius) {
       this.speed.x = -this.speed.x;
--- src/Canvas/Canvas.js
+++ src/Canvas/Canvas.js
@@ -2,29 +2,46 @@ import { useEffect, useRef } from "react";
 import Ball from '../Ball';
 
 
+const getPushAngle = (push) => {
+  const { x1, x2, y1, y2 } = push;
+  return Math.atan2(x2 - x1, y2 - y1);
+}
+
 const Canvas = (props) => {
   const ref = useRef();
 
   useEffect(() => {
     const canvas = ref.current;
     const context = canvas.getContext('2d');
-    // call draw function here
-    const ball1 = new Ball(context, canvas, { x: 100, y: 200 }, { x: 2, y: 3 }, 'red');
-    const ball2 = new Ball(context, canvas, { x: 100, y: 400 }, { x: 2, y: 3 }, 'black');
-    const ball3 = new Ball(context, canvas, {x: 100, y: 400}, {x: 2, y: 3}, 'black');
+
+    const push = {};
+    ref.current.addEventListener('mousedown', (event) => {
+      push.x1 = event.clientX;
+      push.y1 = event.clientY;
+    });
+    ref.current.addEventListener('mouseup', (event) => {
+      push.x2 = event.clientX;
+      push.y2 = event.clientY;
+    });
+
+    const balls = [];
+    for (let i = 0; i < 2; i += 1) {
+      if (i < 1) {
+        balls.push(new Ball(context, canvas, { x: 200 * (i + 0.5), y: 200 }, 'green'))
+      }
+      if (i >= 1) {
+        balls.push(new Ball(context, canvas, { x: 200 * (i + 0.5), y: 400 }, 'red'))
+      }
+    };
+  
     const animate = () => {
       requestAnimationFrame(animate)
       context.clearRect(0, 0, canvas.width, canvas.height)
-      ball1.draw()
-      ball2.draw()
-      ball1.move()
-      ball2.move()
-      if (Ball.getDistance(ball1.position, ball2.position) < ball1.radius + ball2.radius) {
-        ball1.speed.x = -ball1.speed.x
-        ball1.speed.y = -ball1.speed.y
-        ball2.speed.x = -ball2.speed.x
-        ball2.speed.y = -ball2.speed.y
-      }
+      balls.forEach((ball) => {
+        ball.draw()
+        ball.move(push);
+        ball.detectCollision(balls);
+      })
     };
     animate();
   }, []);
